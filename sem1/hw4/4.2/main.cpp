69ea2aa2bd6f37842b7cb74aaae3b19a37a1b930
Anton Chernikov <anton_chernikov1@list.ru>
2018-11-01 21:51:39 +0300

    Added implementation via PhoneBook structure

--- sem1/hw4/4.2/main.cpp
+++ sem1/hw4/4.2/main.cpp
@@ -1,40 +1,89 @@
 #include <iostream>
-#include "functions.h"
+#include "phonebook.h"
 using namespace std;
+
+void executeCommandOne(PhoneBook *phoneBook);
+void executeCommandTwo(PhoneBook *phoneBook);
+void executeCommandThree(PhoneBook *phoneBook);
+void executeCommandFour(PhoneBook *phoneBook, char nameFile[]);
 
 int main()
 {
-    FILE *pilotFile = fopen("pilotFile.txt", "w");
-    FILE *file = fopen("file.txt", "r");
-    copyFiles(file, pilotFile);
+    char nameFile[maxLength] = {"file.txt"};
+    PhoneBook *phoneBook = createPhoneBook();
+    copyFromFile(phoneBook, nameFile);
 
     cout << "0 - escape" << endl;
     cout << "1 - add record" << endl;
     cout << "2 - find number by name" << endl;
     cout << "3 - find name by number" << endl;
-    cout << "4 - save" << endl;
-    cout << endl;
-    int command = 5;
+    cout << "4 - save" << endl << endl;
+    int command = - 1;
     while (command != 0)
     {
-        command = 0;
         cout << "Enter command" << endl;
         cin >> command;
-        if (command == 1)
+        switch (command)
         {
-            commandOne(pilotFile);
-        }
-        if (command == 2)
-        {
-            commandTwo(pilotFile);
-        }
-        if (command == 3)
-        {
-            commandThree(pilotFile);
-        }
-        if (command == 4)
-        {
-            commandFour(pilotFile, file);
+            case 1:
+            {
+                executeCommandOne(phoneBook);
+                break;
+            }
+            case 2:
+            {
+                executeCommandTwo(phoneBook);
+                break;
+            }
+            case 3:
+            {
+                executeCommandThree(phoneBook);
+                break;
+            }
+            case 4:
+            {
+                executeCommandFour(phoneBook, nameFile);
+                break;
+            }
         }
     }
+    deletePhoneBook(phoneBook);
+}
+
+void executeCommandOne(PhoneBook *phoneBook)
+{
+    cout << "Enter name: ";
+    char *name = new char[maxLength] {};
+    cin >> name;
+    cout << "Enter number: ";
+    char *number = new char[maxLength] {};
+    cin >> number;
+    addRecord(phoneBook, name, number, false);
+    cout << "Record added" << endl << endl;
+}
+
+void executeCommandTwo(PhoneBook *phoneBook)
+{
+    cout << "Enter name: ";
+    char *name = new char[maxLength] {};
+    cin >> name;
+    cout << "Its number: ";
+    findNumber(phoneBook, name);
+    cout << endl << endl;
+}
+
+void executeCommandThree(PhoneBook *phoneBook)
+{
+    cout << "Enter number: ";
+    char *number = new char[maxLength] {};
+    cin >> number;
+    cout << "Its name: ";
+    findName(phoneBook, number);
+    cout << endl << endl;
+}
+
+void executeCommandFour(PhoneBook *phoneBook, char nameFile[])
+{
+    copyIntoFile(phoneBook, nameFile);
+    cout << "Data saved" << endl << endl;
 }
---
+++ sem1/hw4/4.2/phonebook.h
@@ -0,0 +1,28 @@
+#pragma once
+
+struct ElementPhoneBook
+{
+    char *name;
+    char *number;
+    bool saved;
+    ElementPhoneBook *next;
+};
+
+struct PhoneBook
+{
+    ElementPhoneBook *first;
+};
+
+PhoneBook *createPhoneBook();
+void addRecord(PhoneBook *phoneBook, char *name, char *number, bool saved);
+void findName(PhoneBook *phoneBook, char *number);
+void findNumber(PhoneBook *phoneBook, char *name);
+void copyFromFile(PhoneBook *phoneBook, char nameFile[]);
+void copyIntoFile(PhoneBook *phoneBook, char nameFile[]);
+void deletePhoneBook(PhoneBook *phoneBook);
+
+void insertSymbol(char *line, char symbol);
+int length(char *line);
+bool isEqual(char *firstLine, char *secondLine);
+
+int const maxLength = 256;
