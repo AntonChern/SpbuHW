f8d50e18f45371902fa20d052a60a350291da403
Anton Chernikov <anton_chernikov1@list.ru>
2018-11-01 21:25:24 +0300

    Added new test1

--- sem1/test1/test1.cpp
+++ sem1/test1/test1.cpp
@@ -1,94 +1,112 @@
 #include <iostream>
 using namespace std;
 
-// difficulty log(n)
-
-void qSort(int, int, int n[]);
-void display(int, int n[]);
+int countSpases(char *str, int length);
+void insertSpase(char *rStr, int rLength, int number);
+void displayRStr(char *rStr, int rLength);
 
 int main()
 {
-    cout << "Enter amount of Vasya's reports" << endl;
-    int lengthV = 0;
-    cin >> lengthV;
-    int *stackV = new int[lengthV] {};
-    cout << "Enter complexity of Vasya's items in descending order" << endl;
-    for (int i = 0; i < lengthV; i++)
+    cout << "Enter length of string" << endl;
+    int length = 0;
+    cin >> length;
+
+    cout << "Enter string" << endl;
+    char *str = new char[length] {};
+    for (int i = 0; i <= length; i++)
     {
-        cin >> stackV[i];
-    }
-
-    cout << "Enter amount of Petya's reports" << endl;
-    int lengthP = 0;
-    cin >> lengthP;
-    int *stackP = new int[lengthP] {};
-    cout << "Enter complexity of Petya's items in descending order" << endl;
-    for (int i = 0; i < lengthP; i++)
-    {
-        cin >> stackP[i];
-    }
-
-    int *stackJoint = new int[lengthV + lengthP] {};
-
-    for (int i = 0; i < lengthV; i++)
-    {
-        stackJoint[i] = stackV[i];
-    }
-    for (int i = lengthV; i < lengthV + lengthP; i++)
-    {
-        stackJoint[i] = stackP[i - lengthV];
-    }
-
-    qSort(0, lengthV + lengthP - 1, stackJoint);
-    cout << "Result - ";
-    display(lengthV + lengthP, stackJoint);
-
-    delete[] stackJoint;
-    delete[] stackV;
-    delete[] stackP;
-}
-
-void qSort(int first, int last, int num[])
-{
-    if (first < last)
-    {
-        int length = last - first + 1;
-        int select = first + length / 2;
-        bool isSorted = false;
-        while (!isSorted)
+        char symbol = '\0';
+        scanf("%c", &symbol);
+        if (i > 0)
         {
-            isSorted = true;
-            for (int i = 0; i < select - first; i++)
-            {
-                if (num[first + i] <= num[select])
-                {
-                    swap(num[first + i], num[select]);
-                    select = first + i;
-                    isSorted = false;
-                    break;
-                }
-            }
-            for (int i = 0; i < last - select; i++)
-            {
-                if (num[last - i] > num[select])
-                {
-                    swap(num[last - i], num[select]);
-                    select = last - i;
-                    isSorted = false;
-                    break;
-                }
-            }
+            str[i - 1] = symbol;
         }
-        qSort(first, select - 1, num);
-        qSort(select + 1, last, num);
     }
-}
 
-void display(int length, int num[])
-{
+    cout << "Enter length of required string" << endl;
+    int rLength = 0;
+    cin >> rLength;
+
+    char *rStr = new char[rLength] {};
     for (int i = 0; i < length; i++)
     {
-        cout << num[i] << " ";
+        rStr[i] = str[i];
     }
-    cout << endl;
+
+    int spases = countSpases(str, length);
+    int reserveSpases = rLength - length;
+    int barrier = length;
+
+    while ((reserveSpases >= spases) && (spases != 0))
+    {
+        for (int i = 0; i < barrier; i++)
+        {
+            if (rStr[i] == ' ')
+            {
+                insertSpase(rStr, rLength, i);
+                reserveSpases--;
+                barrier++;
+                int spasesBetwWords = 0;
+                for (int j = i; rStr[j] == ' '; j++)
+                {
+                    spasesBetwWords++;
+                }
+                i += spasesBetwWords;
+            }
+        }
+    }
+
+    int counter = 1;
+    for (int i = 0; (i < rLength) && (reserveSpases > 0); i++)
+    {
+        if ((rStr[i] == ' ') && (counter > 0))
+        {
+            insertSpase(rStr, rLength, i);
+            reserveSpases--;
+            counter *= -1;
+        }
+    }
+
+    displayRStr(rStr, rLength);
+
+    delete[] str;
+    delete[] rStr;
 }
+
+int countSpases(char *str, int length)
+{
+    int result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] == ' ')
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+void insertSpase(char *rStr, int rLength, int number)
+{
+    int spasesBetwWords = 0;
+    for (int i = number; rStr[i] == ' '; i++)
+    {
+        spasesBetwWords++;
+    }
+    int current = rStr[number + spasesBetwWords];
+    rStr[number + spasesBetwWords] = rStr[number];
+    for (int i = rLength - 1; i >= number + spasesBetwWords; i--)
+    {
+        rStr[i] = rStr[i - 1];
+    }
+    rStr[number + spasesBetwWords + 1] = current;
+}
+
+void displayRStr(char *rStr, int rLength)
+{
+    for (int i = 0; i < rLength; i++)
+    {
+        cout << rStr[i];
+    }
+}
+
