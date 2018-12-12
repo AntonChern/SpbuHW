4c90df1efbe86085823ac0a53e9c8a2afd2d4730
Anton Chernikov <anton_chernikov1@list.ru>
2018-12-13 02:34:24 +0300

    Added second version of test 2.3

--- sem1/test2/test2.3/main.cpp
+++ sem1/test2/test2.3/main.cpp
@@ -1,36 +1,73 @@
 #include <iostream>
-#include "list.h"
 using namespace std;
+
+void reset(bool *included);
+
+const int maxLength = 256;
+const int symbolsABC = 26;
 
 int main()
 {
-    List *list = createList();
-    cout << "Enter n & m: ";
-    int n = 0;
-    int m = 0;
-    cin >> n;
-    cin >> m;
-    const char *nameOfFile = "file.txt";
-    fullListFromFile(list, nameOfFile);
-    sortListDanger(list);
-    cout << endl;
-    cout << "Shot people:" << endl;
-    for (int i = 0; i < n; i++)
+    cout << "Enter sentence: ";
+    char *sentence = new char[maxLength] {};
+    gets(sentence);
+    bool *included = new bool[symbolsABC] {};
+    int amountOfWords = 0;
+
+    int maxLengthOfWord = 0;
+    int lengthOfWord = 0;
+
+    int maxDifferentLetters = 0;
+    int differentLetters = 0;
+
+    for (int i = 0; i < maxLength; i++)
     {
-        displayFirst(list);
-        deleteElement(list);
+        if (!isalpha(sentence[i]) || sentence[i] == '\0')
+        {
+            amountOfWords++;
+            for (int j = 0; j < symbolsABC; j++)
+            {
+                if (included[j])
+                {
+                    differentLetters++;
+                }
+            }
+            if (lengthOfWord > maxLengthOfWord)
+            {
+                maxLengthOfWord = lengthOfWord;
+                maxDifferentLetters = differentLetters;
+            }
+
+            if (sentence[i] == '\0')
+            {
+                break;
+            }
+            while (!isalpha(sentence[i]))
+            {
+                i++;
+            }
+            i--;
+
+            lengthOfWord = 0;
+            differentLetters = 0;
+            reset(included);
+        }
+        else
+        {
+            lengthOfWord++;
+            included[sentence[i] - 'a'] = true;
+        }
     }
-    cout << endl;
-    sortListABC(list);
-    cout << "People sent to Siberia:" << endl;
-    for (int i = 0; i < m; i++)
-    {
-        displayFirst(list);
-        deleteElement(list);
-    }
-    cout << endl;
-    cout << "Unscathed people:" << endl;
-    displayList(list);
-    deleteList(list);
+    cout << "Amount of words = " << amountOfWords << endl;
+    cout << "Number of different letters in the longest word = " << maxDifferentLetters;
+    delete[] sentence;
+    delete[] included;
 }
 
+void reset(bool *included)
+{
+    for (int i = 0; i < symbolsABC; i++)
+    {
+        included[i] = false;
+    }
+}
