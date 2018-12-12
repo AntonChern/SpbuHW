0bc95beb2437ccb78c6120794571bb02ee45e98e
Anton Chernikov <anton_chernikov1@list.ru>
2018-12-13 01:44:12 +0300

    Added deletion of word

--- sem1/hw8/8.3/main.cpp
+++ sem1/hw8/8.3/main.cpp
@@ -3,6 +3,8 @@
 #include "hashtable.h"
 using namespace std;
 
+void nullWord(char *word);
+
 const int maxLength = 256;
 
 int main()
@@ -10,9 +12,10 @@ int main()
     const char *nameOfFile = "file.txt";
     ifstream file(nameOfFile);
     HashTable *hashTable = createHashTable(2);
+    char *word = new char[maxLength] {};
     while (!file.eof())
     {
-        char *word = new char[maxLength] {};
+        nullWord(word);
         char symbol = '\0';
         while (!isalpha(symbol) && !file.eof())
         {
@@ -28,12 +31,20 @@ int main()
             file.get(symbol);
         }
         addRecord(hashTable, word);
-        delete[] word;
     }
+    delete[] word;
     displayWordsAndAmounts(hashTable);
     cout << endl;
     cout << "Load factor = " << loadFactor(hashTable) << endl;
     displaySamplesAndCells(hashTable);
     deleteHashTable(hashTable);
     file.close();
+}
+
+void nullWord(char *word)
+{
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        word[i] = '\0';
+    }
 }
