d632332553fc932f321e7828384f25ba1044d93c
AntonChern <anton_chernikov1@list.ru>
2019-10-27 20:33:28 +0300

    Added task 3.Cannon

---
+++ sem3/hw3/Cannon/src/main/java/com/anton/chernikov/g244/Const.java
@@ -0,0 +1,25 @@
+package com.anton.chernikov.g244;
+
+public abstract class Const {
+    public static final int windowWidth = 1536;
+    public static final int windowHeight = 768;
+    public static final double rotationSpeed = 1.8;
+    public static final double motionSpeed = 8;
+    public static final int shootSpeed = 100;
+    public static final double barrelShift = 18;
+    public static final double minAngle = barrelShift - 90;
+    public static final double maxAngle = barrelShift;
+
+    public static final int ballShiftX = 12;
+    public static final int ballExtraShiftX = 5;
+    public static final int ballLandShiftX = 9;
+    public static final int ballLandShiftY = 12;
+    public static final int cannonShiftX = 21;
+    public static final int cannonExtraShiftX = 2;
+    public static final int cannonShiftY = 31;
+
+    public static final int backWheelShiftX = 29;
+    public static final int backWheelShiftY = 11;
+    public static final int barrelShiftX = 26;
+    public static final int barrelShiftY = 14;
+}
