5ba7843f884836876ce4ab06a8d2044d0f20cde0
AntonChern <anton_chernikov1@list.ru>
2020-03-31 22:39:59 +0400

    Syntax fixed

--- sem4/hw5/Task5_2.hs
+++ sem4/hw5/Task5_2.hs
@@ -3,4 +3,4 @@ module Task5_2 where
 import Prelude
 
 products :: Int -> [Int]
-products n = sequence [[1..n],[1..n]] >>= \(x:y:[]) -> return (x*y)
+products n = sequence [[1..n],[1..n]] >>= \[x,y] -> return (x*y)
