84ec9898140afe29ab39b8833adba5c17902ab6d
AntonChern <anton_chernikov1@list.ru>
2020-04-10 22:56:31 +0400

    Redone

--- sem4/hw5/Task5_3.hs
+++ sem4/hw5/Task5_3.hs
@@ -3,18 +3,8 @@ module Task5_3 where
 import Control.Monad
 import Prelude
 
-fstMax :: [Int] -> Int
-fstMax = middleFst.discardOdd.toTriples
-
-middleFst :: [(Int, Int, Int)] -> Int
-middleFst [] = error "No such elements"
-middleFst list = (\(x, y, z) -> y).head $ list
-
-discardOdd :: [(Int, Int, Int)] -> [(Int, Int, Int)]
-discardOdd = mfilter (\(x, y, z) -> (y > x) && (y > z))
-
-toTriples :: [Int] -> [(Int, Int, Int)]
-toTriples list = join $ zipWithM (\x (y, z) -> [(x, y, z)]) list $ toPairs $ tail list
-
-toPairs :: [Int] -> [(Int, Int)]
-toPairs list = join $ zipWithM (\x y -> [(x, y)]) list $ tail list
+fstMax :: (Ord a) => [a] -> Maybe a
+fstMax (x:tail@(y:z:rest))
+    | rest == [] = condition
+    | otherwise = mplus condition $ fstMax tail
+    where condition = if (y > x) && (y > z) then Just y else Nothing
