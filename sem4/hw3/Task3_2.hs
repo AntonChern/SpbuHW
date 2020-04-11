149532133dd2a04a5dce934857f45350273c81da
AntonChern <anton_chernikov1@list.ru>
2020-04-11 23:09:03 +0400

    Optimized

--- sem4/hw3/Task3_2.hs
+++ sem4/hw3/Task3_2.hs
@@ -2,4 +2,4 @@ module Task3_2 where
 
 import Prelude
 
-series = 1 : 7 : 9 : concat [(10 * x + (series !! 0)) : (10 * x + (series !! 1)) : (10 * x + (series !! 2)) : [] | x <- series ]
+series = zipWith (+) (concatMap ((replicate 3).(10*)) (0:series)) (concatMap (const [1,7,9]) [1..])
