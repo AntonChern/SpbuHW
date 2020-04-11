73016447428f9149862428260b3bd58232089574
AntonChern <anton_chernikov1@list.ru>
2020-04-11 23:53:43 +0400

    Optimized

--- sem4/hw3/Task3_3.hs
+++ sem4/hw3/Task3_3.hs
@@ -2,17 +2,8 @@ module Task3_3 where
 
 import Prelude
 
-diff :: [Int] -> Int
-maxi :: [Int] -> Int
-diff = maxi.diff'
-diff' [] = []
-diff' (element:[]) = []
-diff' (frst:scnd:rest) = (frst + scnd) : diff' (scnd:rest)
-maxi [] = error "Not enough elements"
-maxi series = maxi' series 1 (1, head series)
-maxi' (last:[]) indexCurr (indexMax, maxElem) = if last > maxElem
-                                                then indexCurr + 1
-                                                else indexMax
-maxi' (curr:rest) indexCurr (indexMax, maxElem) = maxi' rest (indexCurr + 1) (if head rest > maxElem
-                                                                              then (indexCurr + 1, head rest)
-                                                                              else (indexMax, maxElem))
+imax :: (Num a, Ord a) => [a] -> Int
+imax l = imax' $ zipWith (+) l $ tail l
+    where imax' :: (Num a, Ord a) => [a] -> Int
+          imax' [] = error "Not enough elements"
+          imax' l@(x:xs) = if x == maximum l then 1 else 1 + (imax' xs)
