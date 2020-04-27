9b3f8a3d1b305d75e3d6358eb429f1540ed63c5e
AntonChern <anton_chernikov1@list.ru>
2020-04-27 06:53:19 +0400

    Added test

--- sem4/hw3/Task3_3.hs
+++ sem4/hw3/Task3_3.hs
@@ -7,3 +7,6 @@ imax l = imax' $ zipWith (+) l $ tail l
     where imax' :: (Num a, Ord a) => [a] -> Int
           imax' [] = error "Not enough elements"
           imax' l@(x:xs) = if x == maximum l then 1 else 1 + (imax' xs)
+
+test :: Bool
+test = imax [4,1,8,3,2,5,6] == 3
