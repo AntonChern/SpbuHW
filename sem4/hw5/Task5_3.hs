e9513cb90764efe359ec3e2e35ae48356abf12e1
AntonChern <anton_chernikov1@list.ru>
2020-04-27 06:47:22 +0400

    Added tests

--- sem4/hw5/Task5_3.hs
+++ sem4/hw5/Task5_3.hs
@@ -8,3 +8,9 @@ fstMax (x:tail@(y:z:rest))
     | rest == [] = condition
     | otherwise = mplus condition $ fstMax tail
     where condition = if (y > x) && (y > z) then Just y else Nothing
+
+test1 :: Bool
+test1 = fstMax [9,1,4,3,5,2] == Just 4
+
+test2 :: Bool
+test2 = fstMax [5,4,3] == Nothing
