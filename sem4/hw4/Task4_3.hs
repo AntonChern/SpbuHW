8cffd70e5ab9dec33b9a550b2952140600442c86
AntonChern <anton_chernikov1@list.ru>
2020-04-27 06:34:11 +0400

    Optimized and added test

--- sem4/hw4/Task4_3.hs
+++ sem4/hw4/Task4_3.hs
@@ -2,12 +2,14 @@ module Task4_3 where
 
 import Prelude
 
-data BinTree a = Empty | Leaf a | Branch (BinTree a) a (BinTree a)
+data BinTree a = Empty | Branch (BinTree a) a (BinTree a)
 
 instance Foldable BinTree where
     foldr f b Empty = b
-    foldr f b (Leaf a) = f a b
     foldr f b (Branch l a r) = foldr f (f a (foldr f b r)) l
 
 getList :: BinTree a -> [a]
 getList = foldr (:) []
+
+test :: Bool
+test = getList (Branch (Branch Empty 9 Empty) 3 (Branch (Branch Empty 5 Empty) 7 (Branch Empty 4 Empty))) == [9,3,5,7,4]
