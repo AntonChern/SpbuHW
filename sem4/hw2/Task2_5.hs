b6d19fb1975da42f07f6faf44478a8a86f0ab7f6
AntonChern <anton_chernikov1@list.ru>
2020-04-27 07:01:59 +0400

    Added test

--- sem4/hw2/Task2_5.hs
+++ sem4/hw2/Task2_5.hs
@@ -6,3 +6,6 @@ sum3 :: [Int] -> [Int] -> [Int] -> [Int]
 sum3 l1 l2 l3 = zipWith3 (\x y z -> x + y + z) (expand l1) (expand l2) (expand l3)
     where expand list = list ++ replicate (mlen - length list) 0
           mlen = maximum [length l1, length l2, length l3]
+
+test :: Bool
+test = sum3 [1,2,3] [4,5] [6] == [11,7,3]
