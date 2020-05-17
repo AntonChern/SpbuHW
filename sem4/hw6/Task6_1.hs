0a8f374b001455025c70e4d0cab584a75c7b95e1
AntonChern <anton_chernikov1@list.ru>
2020-05-17 21:05:58 +0400

    Redone polynomial showing

--- sem4/hw6/Task6_1.hs
+++ sem4/hw6/Task6_1.hs
@@ -4,10 +4,23 @@ import Prelude
 
 data Polynomial a = Empty | a :.: Polynomial a
 
-instance (Show a) => Show (Polynomial a) where
+instance (Show a, Num a, Ord a) => Show (Polynomial a) where
     show Empty = ""
-    show (a:.:Empty) = show a
-    show (a:.:rest) = show a ++ "+x*(" ++ show rest ++ ")"
+    show p = if head result == '+' then tail result else result
+        where result = show' p 0
+              show' :: (Show a, Num a, Ord a) => Polynomial a -> Int -> String
+              show' Empty _ = ""
+              show' (a:.:rest) 0
+                | a == fromInteger 0 = show' rest 1
+                | otherwise = show a ++ show' rest 1
+              show' (a:.:rest) 1
+                | a == fromInteger 0 = show' rest 2
+                | a < fromInteger 0 = (if a == fromInteger (-1) then "-" else show a ++ "*") ++ "x" ++ show' rest 2
+                | otherwise = "+" ++ (if a == fromInteger 1 then "" else show a ++ "*") ++ "x" ++ show' rest 2
+              show' (a:.:rest) n
+                | a == fromInteger 0 = show' rest (n + 1)
+                | a < fromInteger 0 = (if a == fromInteger (-1) then "-" else show a ++ "*") ++ "x^" ++ show n ++ show' rest (n + 1)
+                | otherwise = "+" ++ (if a == fromInteger 1 then "" else show a ++ "*") ++ "x^" ++ show n ++ show' rest 2
 
 instance (Num a) => Num (Polynomial a) where
     a + Empty = a
