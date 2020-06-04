d92d09af2a29da1ee7235688de5f1b74c27d506e
AntonChern <anton_chernikov1@list.ru>
2020-06-05 01:16:48 +0400

    Removed unnecessary variable name

--- sem4/hw2/Task2_4.hs
+++ sem4/hw2/Task2_4.hs
@@ -3,5 +3,5 @@ module Task2_4 where
 import Prelude
 
 firstIndex :: Integer -> [Integer] -> Integer
-firstIndex a [] = error "No such element"
+firstIndex _ [] = error "No such element"
 firstIndex a (x:xs) = if a == x then 1 else 1 + firstIndex a xs
