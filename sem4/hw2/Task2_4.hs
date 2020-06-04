068a8b005de91d108e0b60f11b14b92b5f3b02eb
AntonChern <anton_chernikov1@list.ru>
2020-06-04 16:25:35 +0400

    Added case handling without element

--- sem4/hw2/Task2_4.hs
+++ sem4/hw2/Task2_4.hs
@@ -3,4 +3,5 @@ module Task2_4 where
 import Prelude
 
 firstIndex :: Integer -> [Integer] -> Integer
+firstIndex a [] = error "No such element"
 firstIndex a (x:xs) = if a == x then 1 else 1 + firstIndex a xs
